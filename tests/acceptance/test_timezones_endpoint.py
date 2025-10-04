c6dbea3c96b819477796f0b00bbceb015e9c33af
Konrad Podloucky <konrad@crunchy-frog.org>
2025-10-04 19:22:35 +0000

    Acceptance Test for `/timezones` With Coordinates

--- tests/acceptance/test_timezones_endpoint.py
+++ tests/acceptance/test_timezones_endpoint.py
@@ -13,3 +13,15 @@ def test_timezones_endpoint_without_parameter_returns_all_timezones():
     timezones: list[str] = response.json()
     assert isinstance(timezones, list)
     assert len(timezones) > 0
+
+
+def test_timezones_endpoint_with_lon_and_lat_returns_timezone_of_coordinate():
+    client = TestClient(app)
+
+    # When the timezone for the coordinates of Vienna is requested,
+    response = client.get(TIMEZONES_ENDPOINT, params={"lat": 48.2081, "lon": 16.3713})
+
+    # Then the timezone "Europe/Vienna" will be returned.
+    assert response.is_success
+    timezone: str = response.json()
+    assert timezone == "Europe/Vienna"
