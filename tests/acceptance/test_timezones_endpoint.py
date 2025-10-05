6018eb6179c3434b3e5220410a30814cacbe6b21
Konrad Podloucky <konrad@crunchy-frog.org>
2025-10-05 15:05:34 +0000

    Refactor: Reorganize Acceptance Tests

--- tests/acceptance/test_timezones_endpoint.py
+++ tests/acceptance/test_timezones_endpoint.py
@@ -3,46 +3,46 @@ from fastapi.testclient import TestClient
 from timezone_service.main import TIMEZONES_ENDPOINT
 
 
-def test_timezones_endpoint_without_parameter_returns_all_timezones(test_client: TestClient):
-    # When all timezones are requested,
-    response = test_client.get(TIMEZONES_ENDPOINT)
+class TestTimezonesEndpointWithoutParameters:
+    def test_returns_all_timezones(self, test_client: TestClient):
+        # When all timezones are requested,
+        response = test_client.get(TIMEZONES_ENDPOINT)
 
-    # Then a list of timezone strings will be returned.
-    assert response.is_success
-    timezones: list[str] = response.json()
-    assert isinstance(timezones, list)
-    assert len(timezones) > 0
-
-
-def test_timezones_endpoint_with_lon_and_lat_returns_timezone_of_coordinate(test_client: TestClient):
-    # When the timezone for the coordinates of Vienna is requested,
-    response = test_client.get(TIMEZONES_ENDPOINT, params={"lat": 48.2081, "lon": 16.3713})
-
-    # Then the timezone "Europe/Vienna" will be returned.
-    assert response.is_success
-    timezone: str = response.json()
-    assert timezone == "Europe/Vienna"
+        # Then a list of timezone strings will be returned.
+        assert response.is_success
+        timezones: list[str] = response.json()
+        assert isinstance(timezones, list)
+        assert len(timezones) == 418
 
 
-def test_timezones_endpoint_for_point_in_ocean_returns_nautical_timezone(test_client: TestClient):
-    # When the timezone for a point in the Atlantic is requested,
-    response = test_client.get(TIMEZONES_ENDPOINT, params={"lat": -10, "lon": -29})
+class TestTimezonesEndpointWithLonLat:
+    def test_returns_timezone_of_coordinate(self, test_client: TestClient):
+        # When the timezone for the coordinates of Vienna is requested,
+        response = test_client.get(TIMEZONES_ENDPOINT, params={"lat": 48.2081, "lon": 16.3713})
 
-    # Then the nautical timezone using the "Etc/GMT" prefix is returned.
-    assert response.is_success
-    timezone: str = response.json()
-    assert timezone == "Etc/GMT+2"
+        # Then the timezone "Europe/Vienna" will be returned.
+        assert response.is_success
+        timezone: str = response.json()
+        assert timezone == "Europe/Vienna"
 
+    def test_returns_nautical_timezone_for_point_in_ocean(self, test_client: TestClient):
+        # When the timezone for a point in the Atlantic is requested,
+        response = test_client.get(TIMEZONES_ENDPOINT, params={"lat": -10, "lon": -29})
 
-def test_timezones_endpoint_returns_nautical_timzeones_for_uninhabited_territory(test_client: TestClient):
-    # Given the uninhabited territory of "Steventon Island"
-    # 🐧🐧🐧 (sorry penguins, you don't count),
-    steventon_island_coords = {"lat": -77.25, "lon": -148.25}
+        # Then the nautical timezone using the "Etc/GMT" prefix is returned.
+        assert response.is_success
+        timezone: str = response.json()
+        assert timezone == "Etc/GMT+2"
 
-    # When the timezone for the coordinates of Steventon Island is requested
-    response = test_client.get(TIMEZONES_ENDPOINT, params=steventon_island_coords)
+    def test_returns_nautical_timezones_for_uninhabited_territory(self, test_client: TestClient):
+        # Given the uninhabited territory of "Steventon Island"
+        # 🐧🐧🐧 (sorry penguins, you don't count),
+        steventon_island_coords = {"lat": -77.25, "lon": -148.25}
 
-    # Then the corresponding nautical timezone will be returned.
-    assert response.is_success
-    timezone: str = response.json()
-    assert timezone == "Etc/GMT+10"
+        # When the timezone for the coordinates of Steventon Island is requested
+        response = test_client.get(TIMEZONES_ENDPOINT, params=steventon_island_coords)
+
+        # Then the corresponding nautical timezone will be returned.
+        assert response.is_success
+        timezone: str = response.json()
+        assert timezone == "Etc/GMT+10"
