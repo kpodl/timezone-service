16063fcd0a1da703e6792586802cc56ca55b8948
Konrad Podloucky <konrad@crunchy-frog.org>
2025-10-05 10:25:54 +0000

    Add `TimezoneDatabase.get_timezone_for_point()`

--- src/timezone_service/timezone_db.py
+++ src/timezone_service/timezone_db.py
@@ -2,10 +2,13 @@ from typing import Self
 
 import geopandas
 from geopandas import GeoDataFrame
+from shapely import Point
 
 
 class TimezoneDatabase:
     TIMEZONE_HEADER = "TZID"
+    GEOMETRY_HEADER = "geometry"  # this is a standard GeoDataFrame header
+    CRS = "EPSG:4326"  # coordinate reference system, mainly used in tests
 
     _timezones_df: GeoDataFrame
 
@@ -19,3 +22,13 @@ class TimezoneDatabase:
 
     def get_all_timezones(self) -> set[str]:
         return set(self._timezones_df[self.TIMEZONE_HEADER])
+
+    def get_timezone_for_point(self, point: Point) -> str | None:
+        # Using `predicate = "within"` ensures that the result is unique under the
+        # assumption that the geometries do not overlap (disjunct except for border).
+        timezone_indexes = self._timezones_df.sindex.query(point, predicate="within")
+        matching_timezones = self._timezones_df.iloc[timezone_indexes][self.TIMEZONE_HEADER].to_list()
+        if matching_timezones:
+            return matching_timezones[0]
+        else:
+            return None
--- tests/unit/test_timezone_db.py
+++ tests/unit/test_timezone_db.py
@@ -1,10 +1,41 @@
+import pytest
 from geopandas import GeoDataFrame
-from shapely import Point
+from shapely import Point, Polygon
 
 from timezone_service import TimezoneDatabase
 
 
 class TestTimezoneDatabase:
+    TZ_1 = "TZ1"
+    TZ_2 = "TZ2"
+    TZ_3 = "TZ3"
+    ALL_TZ = (TZ_1, TZ_2, TZ_3)
+    POINT_IN_TZ_1 = Point(3, 5)
+    POINT_IN_TZ_2 = Point(2, -2)
+    POINT_ON_BORDER_OF_TZ_1 = Point(0, 0)
+
+    @pytest.fixture
+    def geo_df(self) -> GeoDataFrame:
+        return GeoDataFrame(
+            {
+                TimezoneDatabase.TIMEZONE_HEADER: [
+                    self.TZ_1,
+                    self.TZ_2,
+                    self.TZ_3,
+                ],
+                TimezoneDatabase.GEOMETRY_HEADER: [
+                    Polygon(((0, 0), (10, 0), (0, 10))),
+                    Polygon(((0, 0), (3, 0), (3, -2), (0, -5))),
+                    Polygon(((0, -5), (-5, -5), (0, -10))),
+                ],
+            },
+            crs=TimezoneDatabase.CRS,
+        )
+
+    @pytest.fixture
+    def tz_db(self, geo_df: GeoDataFrame) -> TimezoneDatabase:
+        return TimezoneDatabase(geo_df)
+
     def test_get_all_timezones_returns_all_unique_timezones(self):
         tz_db = TimezoneDatabase(
             GeoDataFrame(
@@ -14,13 +45,13 @@ class TestTimezoneDatabase:
                         "Europe/Vienna",
                         "Europe/Vienna",
                     ],
-                    "geometry": [
+                    TimezoneDatabase.GEOMETRY_HEADER: [
                         Point(0, 0),
                         Point(0, 1),
                         Point(1, 1),
                     ],
                 },
-                crs="EPSG:4326",
+                crs=TimezoneDatabase.CRS,
             )
         )
 
@@ -28,3 +59,18 @@ class TestTimezoneDatabase:
 
         assert len(all_timezones_list) > 0
         assert len(all_timezones_list) == len(set(all_timezones_list))
+
+    @pytest.mark.parametrize(
+        ("point", "resulting_tz"),
+        [
+            (POINT_IN_TZ_1, TZ_1),
+            (POINT_IN_TZ_2, TZ_2),
+            (POINT_ON_BORDER_OF_TZ_1, None),
+        ],
+    )
+    def test_get_timezone_for_point_returns_timezone_of_geometry_containing_point(
+        self, tz_db: TimezoneDatabase, point: Point, resulting_tz: str | None
+    ):
+        timezone = tz_db.get_timezone_for_point(point)
+
+        assert timezone == resulting_tz
